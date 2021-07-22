71aa7666835f782050a1a364f3dea320f61ad0df
yuki <s1932058jl@s.chibakoudai.jp>
2021-07-22 23:18:10 +0900

    APIで天気取得

---
+++ src/components/Results.tsx
@@ -0,0 +1,28 @@
+type ResultsPropsType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperature: string;
+        conditionText: string;
+        icon: string;
+    }
+}
+
+const Results = ({ results }: ResultsPropsType) => {
+    const { cityName, country, temperature, conditionText, icon } = results;
+    return (
+        <div>
+            {cityName && <div>{cityName}</div>}
+            {country && <div>{country}</div>}
+            {temperature && <div>{temperature}<span>℃</span></div>}
+            {conditionText &&
+                <div>
+                    <img src={icon} alt="icon" />
+                    <span>{conditionText}</span>
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Results;
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+const Title = () => {
+    return (
+        <h1>Weather App</h1>
+    );
+};
+
+export default Title;
